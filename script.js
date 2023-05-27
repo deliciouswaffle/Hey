f76a99eb7dccca12c621fed7349685a29ace3caf
deepl <131652318+deliciouswaffle@users.noreply.github.com>
2023-05-27 09:45:16 +0900

    Hey!

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function changeContent(){
     let as = document.querySelectorAll('a');
     as.forEach(async v => {
-        if (v.innerHTML.startsWith('https://youtu.be/')) {
-            let a = v.innerHTML.slice(17);
+        if (v.innerHTML.startsWith('https://youtu.be/') || v.innerHTML.startsWith('https://www.youtube.com/watch')){
+            if (v.innerHTML.startsWith('https://youtu.be/')) {
+                id = v.innerHTML.slice(17);
+            } else if (v.innerHTML.startsWith('https://www.youtube.com/watch')) {
+                id = v.innerHTML.slice(32);
+            }
 
             //const iframe = `<br><div id='url'>https://youtu.be/${a}</div><iframe width="426" height="240" src="https://www.youtube.com/embed/${a}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>`;
-            const iframe = `<br><iframe width="426" height="240" src="https://www.youtube.com/embed/${a}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>`;
+            const iframe = `<br><iframe width="426" height="240" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>`;
             v.innerHTML = iframe;
         }
     });
