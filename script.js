425cb5530de22c50570182398d15f4f5aa617420
deepl <131652318+deliciouswaffle@users.noreply.github.com>
2023-05-27 14:18:07 +0900

    Hey!

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function changeContent() {
 
             //const iframe = `<br><div id='url'>https://youtu.be/${a}</div><iframe width="426" height="240" src="https://www.youtube.com/embed/${a}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>`;
             if (!id.includes('"')) {
-                const iframe = `<br><iframe width="426" height="240" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br>`;
+                const iframe = `<br><br><iframe width="426" height="240" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br>`;
                 v.innerHTML = iframe;
             }
         }
